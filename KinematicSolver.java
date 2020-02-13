307223fd9592bfe52abcd31ac9c4375f82867d75
TimofeyBorozdin <43412614+TimofeyBorozdin@users.noreply.github.com>
2020-02-13 10:34:22 +0100

    Update KinematicSolver.java
    extra functionality added

--- KinematicSolver.java
+++ KinematicSolver.java
@@ -34,35 +34,42 @@ public class KinematicSolver {
         }
     }
     
-    static void Exercise() {     
-        System.out.println();
-        System.out.println("----EXERCISE: Hello!");
-                    
+    static void Exercise() {                
         Scanner vars = new Scanner(System.in);
-        System.out.println("----EXERCISE: Please enter velocity of an object");
-        double V = vars.nextFloat();
-        System.out.println("----EXERCISE: Please enter initial height");
-        double h = vars.nextFloat();
-        System.out.println("----EXERCISE: Please enter value of an angle to the horizon");
-        double a = vars.nextFloat();
-            
         double g = 9.8f;
-            
+        double V;
+        double h;
+        double a;
+        
         //------------------------------------------
-        double v;
-        double u;
+        double verticalInitial;
+        double verticalFinal;
+        double horizontal;
+        double Vfin;
         double dH;
         double H;
         double t;
         double T;
         double S;
+        double b; //(final velocity angle)
         //------------------------------------------
+        
+        
+        System.out.println();
+        System.out.println("----EXERCISE: Hello!");
+        System.out.println("----EXERCISE: Please enter velocity of an object");
+        V = vars.nextFloat();
+        System.out.println("----EXERCISE: Please enter initial height");
+        h = vars.nextFloat();
+        System.out.println("----EXERCISE: Please enter value of an angle to the horizon");
+        a = vars.nextFloat();
+        
             
-        v = Math.abs(V * Math.sin(a * Math.PI / 180)); //vertical
-        u = Math.abs(V * Math.cos(a * Math.PI / 180)); //horizontal
-        dH = Math.pow(v, 2) / (2 * g);
+        verticalInitial = Math.abs(V * Math.sin(a * Math.PI / 180)); //vertical
+        horizontal = Math.abs(V * Math.cos(a * Math.PI / 180)); //horizontal
+        dH = Math.pow(verticalInitial, 2) / (2 * g);
         H = h + dH;
-        t = v / g;
+        t = verticalInitial / g;
             
         if(h == 0) {
             T = 2 * t;
@@ -71,17 +78,20 @@ public class KinematicSolver {
             T = t + Math.sqrt(2 * H / g);
         }
             
-        S = u * T;
-                
+        S = horizontal * T;
+        
+        verticalFinal = verticalInitial + (g * Math.sqrt(2 * H / g));
+        Vfin = Math.sqrt(Math.pow(horizontal, 2) + Math.pow(verticalFinal, 2));
+        b = Math.atan(verticalFinal/horizontal * 180/Math.PI);
         //------------------------------------------
         //rounding to 0.00 form
-        v *= 100;
-        v = Math.round(v);
-        v /= 100;
+        verticalInitial *= 100;
+        verticalInitial = Math.round(verticalInitial);
+        verticalInitial /= 100;
         //rounding to 0.00 form
-        u *= 100;
-        u = Math.round(u);
-        u /= 100;
+        horizontal *= 100;
+        horizontal = Math.round(horizontal);
+        horizontal /= 100;
         //rounding to 0.00 form
         H *= 100;
         H = Math.round(H);
@@ -94,13 +104,30 @@ public class KinematicSolver {
         S *= 100;
         S = Math.round(S);
         S /= 100;
+        //rounding to 0.00 form
+        verticalFinal *= 100;
+        verticalFinal = Math.round(verticalFinal);
+        verticalFinal /= 100;
+        //rounding to 0.00 form
+        Vfin *= 100;
+        Vfin = Math.round(Vfin);
+        Vfin /= 100;
+        //rounding to 0.00 form
+        b *= 100;
+        b = Math.round(b);
+        b /= 100;
         //------------------------------------------
         System.out.println();
-        System.out.println("----EXERCISE: The horisontal component of the initial velocity is " + u);
-        System.out.println("----EXERCISE: The vertical component of the initial velocity is " + v);
+        System.out.println("----EXERCISE: The horisontal component of the initial velocity is " + horizontal);
+        System.out.println("----EXERCISE: The vertical component of the initial velocity is " + verticalInitial);
         System.out.println("----EXERCISE: The maximal height that object reaches is " + H);
         System.out.println("----EXERCISE: The total time of the flight is " + T);
-        System.out.println("----EXERCISE: The total horisontal displacement is " + S);      
+        System.out.println("----EXERCISE: The total horisontal displacement is " + S);
+        
+        System.out.println("----EXERCISE: The final vertical velocity is " + verticalFinal);
+        System.out.println("----EXERCISE: The final velocity is " + Vfin);
+        System.out.println("----EXERCISE: The angle of the final velocity to the horizon is " + b + " degrees");
+        
         System.out.println();
         System.out.println("----EXERCISE: The End.");
         System.out.println();
